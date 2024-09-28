f81a7a200f84c8626da67d25709929a87ee9d3f6
yacine bouklif <y.bouklif@it-students.fr>
2024-09-28 10:49:29 +0200

    Finalisation Api Restful

--- ExpressJs/src/index.mjs
+++ ExpressJs/src/index.mjs
@@ -1,27 +1,38 @@
 import express from 'express';
 import Joi from "joi"
 
-
 const app = express()
 
 app.use(express.json())
 
-const PORT = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-    console.log(`Demarre sur le port : ${PORT}`)
+app.listen(port, () => {
+    console.log(`Demarre sur le port : ${port}`)
 })
 
 const users = [
     {id : 1, Nom: "yacine"},
-    {id : 2, Nom: "yacine"},
-    {id : 3, Nom: "yacine"},
+    {id : 2, Nom: "norhane"},
+    {id : 3, Nom: "Samsung"},
 ]
 
-
+app.get("/", (req,res) => {
+    res.send("Bienvenue!")
+})
 
 app.get("/api/utilisateurs", (req,res) => {
     res.send(users)
+})
+
+app.get("/api/utilisateurs/:id",(req,res) => {
+    const parsedId = parseInt(req.params.id)
+ const trouverId = users.find((user) => user.id === parsedId)
+
+ if(isNaN(parsedId)) return res.send( " Ce n'est pas un nombre ")
+ if(!trouverId) return res.status(404).send("Erreur de Id")
+ 
+ res.send(trouverId)
 })
 
 app.post('/api/utilisateurs', (req, res) => {
@@ -30,20 +41,53 @@ app.post('/api/utilisateurs', (req, res) => {
     });
 
     const validationResult = schema.validate(req.body);
+    console.log(validationResult)
 
     if (validationResult.error) {
         return res.status(400).send("Rentrez un nom valide");
     }
-
    
     const user = {
         id: users.length + 1,
-        nom: req.body.nom  
+        nom: req.body.Nom  
     };
     users.push(user);
     res.send(user);
 });
 
-app.get("/", (req,res) => {
-    res.send("Bienvenue!")
+
+app.put('/api/utilisateurs/:id', (req,res) => {
+    // Trouvez les id et gérer les erreurs 
+    const parsedId = parseInt(req.params.id);
+    const trouverId = users.find((user) => user.id === parsedId);
+
+    if (isNaN(parsedId)) return res.status(400).send("Ce n'est pas un nombre");
+    if (!trouverId) return res.status(404).send("Erreur de Id");
+// Validaiton avec les erreurs 
+    const schema = Joi.object({
+        Nom: Joi.string().min(3).required()
+    });
+
+    const validationResult = schema.validate(req.body);
+    console.log(validationResult);
+
+    if (validationResult.error) {
+        return res.status(400).send("Rentrez un nom valide");
+    }
+// Mettre a jour  
+    trouverId.Nom = req.body.Nom;
+    res.send(trouverId);
+})
+
+
+app.delete('/api/utilisateurs/:id', (req,res) => {
+    const parsedId = parseInt(req.params.id);
+    const trouverId = users.find((user) => user.id === parsedId);
+
+    if (isNaN(parsedId)) return res.status(400).send("Ce n'est pas un nombre");
+    if (!trouverId) return res.status(404).send("Erreur de Id");
+
+    const index = users.indexOf(trouverId)
+    users.splice(index, 1)
+    res.send(trouverId)
 })
